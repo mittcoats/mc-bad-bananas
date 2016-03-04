d4d850b13f33763d22ad30077c737bf187701ce4
Mitt Coats <mittcoats@gmail.com>
2016-03-04 15:20:32 -0500

    update production.rb

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,6 +20,7 @@ class MoviesController < ApplicationController
   # GET /movies/1.json
   def show
     @reviews = Review.where(movie_id: @movie.id).order("created_at DESC")
+
     if @reviews.blank?
       @avg_review = 0
     else
